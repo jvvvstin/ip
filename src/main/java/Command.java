c175f13370fe70e7aa5194439998489ec48fdf07
Justin <justin80@live.com.sg>
2025-08-16 17:50:38 +0800

    Add Command enum

---
+++ src/main/java/Command.java
@@ -0,0 +1,29 @@
+public enum Command {
+    BYE("bye"),
+    LIST("list"),
+    MARK("mark"),
+    UNMARK("unmark"),
+    TODO("todo"),
+    DEADLINE("deadline"),
+    EVENT("event"),
+    DELETE("delete");
+
+    private final String keyword;
+
+    Command(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getKeyword() {
+        return this.keyword;
+    }
+
+    public static Command getCommand(String keyword) throws ButtercupException {
+        for (Command command : Command.values()) {
+            if (keyword.startsWith(command.getKeyword())) {
+                return command;
+            }
+        }
+        throw new ButtercupException("Invalid command '" + keyword + "'. Please try again.");
+    }
+}
