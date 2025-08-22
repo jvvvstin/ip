ce65ec2ec610ae06f78589a848106995f16deed6
Justin <justin80@live.com.sg>
2025-08-22 17:28:14 +0800

    Update text UI test to be correct

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -20,7 +20,7 @@ then
 fi
 
 # run the program, feed buttercup.commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Buttercup < input.txt > ACTUAL.TXT
+java -classpath ../bin buttercup/Buttercup < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
