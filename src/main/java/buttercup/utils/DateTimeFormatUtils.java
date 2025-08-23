6a4b94f46c647e266cdb99d56c8e85045c1a3a55
Justin <justin80@live.com.sg>
2025-08-23 17:52:59 +0800

    Add JavaDoc for DateTimeFormatUtils class

--- src/main/java/buttercup/utils/DateTimeFormatUtils.java
+++ src/main/java/buttercup/utils/DateTimeFormatUtils.java
@@ -6,11 +6,26 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+/**
+ * A util class for processing date time in <code>String</code> formats
+ * and converting Strings in accepted date time formats to a
+ * <code>LocalDateTime</code> object. This util class also formats a
+ * <code>LocaleDateTime</code> into a <code>String</code> in the format
+ * <code>MMM dd yyyy HHmm</code> e.g. Aug 06 2025 1400.
+ */
 public class DateTimeFormatUtils {
     private static final DateTimeFormatter FORMATTER_1 = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
     private static final DateTimeFormatter FORMATTER_2 = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
     private static final String OUTPUT_FORMAT = "MMM dd yyyy HHmm";
 
+    /**
+     * Returns a <code>LocalDateTime</code> object if the <code>dateTimeString</code>
+     * is of a valid/accepted date time format.
+     * @param dateTimeString The date time String to be converted
+     * @return A <code>LocalDateTime</code> object based on the date time
+     * String provided
+     * @throws ButtercupException If the date time String provided is not of an accepted format
+     */
     public static LocalDateTime getLocalDateTimeFromString(String dateTimeString) throws ButtercupException {
         try {
             return LocalDateTime.parse(dateTimeString, FORMATTER_1);
@@ -26,6 +41,12 @@ public class DateTimeFormatUtils {
         }
     }
 
+    /**
+     * Parses a <code>LocalDateTime</code> object into a <code>String</code>
+     * in the format <code>MMM dd yyyy HHmm</code>.
+     * @param dateTime The <code>LocalDateTime</code> object to be parsed.
+     * @return A String representation of the date time provided.
+     */
     public static String formatDateTime(LocalDateTime dateTime) {
         return dateTime.format(DateTimeFormatter.ofPattern(OUTPUT_FORMAT));
     }
