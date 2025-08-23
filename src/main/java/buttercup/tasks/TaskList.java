8f476d38bb8f1e328efca7418c2877bba5299a4a
Justin <justin80@live.com.sg>
2025-08-24 00:05:05 +0800

    Add JavaDoc for TaskList class

--- src/main/java/buttercup/tasks/TaskList.java
+++ src/main/java/buttercup/tasks/TaskList.java
@@ -2,6 +2,11 @@ package buttercup.tasks;
 
 import java.util.List;
 
+/**
+ * Represents a list of Tasks that the user has to complete. Each Task
+ * could be a Todo, Deadline or Event object that the user has to
+ * complete.
+ */
 public class TaskList {
     private List<Task> tasks;
 
@@ -9,30 +14,67 @@ public class TaskList {
         this.tasks = tasks;
     }
 
+    /**
+     * Adds the specified <code>Task</code> object into the list
+     * of tasks.
+     * @param task The Task that is to be added into the list.
+     * @see Task
+     */
     public void addTask(Task task) {
         this.tasks.add(task);
     }
 
+    /**
+     * Removes the specified <code>Task</code> object from the list
+     * of tasks.
+     * @param task The task that is to be removed from the list.
+     * @see Task
+     */
     public void removeTask(Task task) {
         this.tasks.remove(task);
     }
 
+    /**
+     * Returns the total number of Tasks in the list.
+     * @return An <code>int</code> representing the total number of
+     * tasks in the list.
+     */
     public int getSize() {
         return this.tasks.size();
     }
 
+    /**
+     * Returns the <code>Task</code> at the specified index from
+     * the current list of tasks.
+     * @param index The index of the task to be removed.
+     * @return The task previously at the specified position.
+     */
     public Task getTask(int index) {
         return this.tasks.get(index);
     }
 
+    /**
+     * Returns the list of Tasks currently.
+     * @return A list containing all the Tasks.
+     */
     public List<Task> getTasks() {
         return this.tasks;
     }
 
+    /**
+     * Returns a <code>boolean</code> value representing whether the task
+     * list is empty.
+     * @return A <code>boolean</code> value representing whether the task
+     * list is empty.
+     */
     public boolean isEmpty() {
         return this.tasks.isEmpty();
     }
 
+    /**
+     * Returns a <code>String</code> representation of the TaskList object.
+     * @return A <code>String</code> representation of the TaskList object.
+     */
     @Override
     public String toString() {
         int taskNumber = 1;
